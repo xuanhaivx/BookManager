2682b70e620ccd26f9da89d86c009197e4f989a8
Admin <Admin@ADMIN>
2024-05-16 21:52:15 +0700

    Add project files.

---
+++ BookManager/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManager.Controllers
+{
+    public class BookController : Controller
+    {
+        // Static list to simulate a database
+        private static List<Book> books = new List<Book>();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View(books);
+        }
+
+        // GET: Book/AddNew
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+
+        // POST: Book/AddNew
+        [HttpPost]
+        public ActionResult AddNew(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                book.IdBook = books.Count + 1;
+                books.Add(book);
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+    }
+}
---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookManager.Models
+{
+    public class Book
+    {
+        public int IdBook { get; set; }
+        public string TitleBook { get; set; }
+        public string AuthorBook { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
